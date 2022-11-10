f3dbfc4b1497b7bc35f2c4e4b6ef3897a28f77d6
wibox <francesco.info.pagano@gmail.com>
2022-11-10 14:51:09 +0100

    added initial structure

---
+++ main/Preprocessing/combiner.py
@@ -0,0 +1,9 @@
+def Combiner():
+    def __init__():
+        pass
+
+    def combine(self, pre, post, mask):
+        pass
+
+    def save_combined_img(self, img):
+        pass
---
+++ main/Preprocessing/dataloader.py
@@ -0,0 +1,24 @@
+"""
+QUESTO FILE CONTIENE LA CLASSE NECESSARIA A SCANNERIZZARE TUTTE LE INFORMAZIONI RELATIVE AL DATASET
+A NOSTRA DISPOSIZIONE E SALVA TUTTI I RECORD IN UN FILE .CSV APPOSITAMENTE FORMATTATO(cocks)
+"""
+class DatasetScanner():
+    def __init__(self):
+        pass
+
+    def scan_master_folder(self):
+        pass
+
+    def log_to_file(self, filename):
+        pass
+
+class DatasetFormatter():
+    def __init__(self):
+        pass
+
+    def check_dimensions(self):
+        pass
+
+    def format_dimensions(self):
+        pass
+
