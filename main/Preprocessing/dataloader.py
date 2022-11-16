61c7860ff9234f974bc190f6a14dccd5ebb03e2a
wibox <francesco.info.pagano@gmail.com>
2022-11-16 15:06:38 +0100

    DataScanner class completed.

--- main/Preprocessing/dataloader.py
+++ main/Preprocessing/dataloader.py
@@ -1,19 +1,6 @@
-"""
-QUESTO FILE CONTIENE LA CLASSE NECESSARIA A SCANNERIZZARE TUTTE LE INFORMAZIONI RELATIVE AL DATASET
-A NOSTRA DISPOSIZIONE E SALVA TUTTI I RECORD IN UN FILE .CSV APPOSITAMENTE FORMATTATO(cocks)
-
-IL WORKFLOW E' IL SEGUENTE:
-    - PRENDO IMMAGINE DA /pre (RICOSTRUIRE TILES)
-    - PRENDO IMMAGINE DA /post (RICOSTRUIRE TILES)
-    - ASSEMBLO PRE E POST E LI METTO IN /complete (sono già tile-assembled)
-    - APPLICO LA MASCHERA A QUELLA COMPLETA
-
-"""
-
 import os
-import sys
-import glob
-
+import json
+from typing import *
 
 class DatasetScanner():
     """
@@ -22,27 +9,63 @@ class DatasetScanner():
     In questo modo evitiamo di mantenere in memoria a runtime 520 giga di informazioni sul path assoluto
     E processiamo riga per riga il csv risultate. (generato da log_to_file()).
     """
-    def __init__(self, master_folder_path=None, log_file_path=None, verbose=0, valid_list=None, ignore_list=None):
+    def __init__(self, master_folder_path=None, log_file_path=None, validation_file_path=None, valid_list=None, ignore_list=None, verbose=0):
         self.master_folder_path=master_folder_path
         self.log_file_path=log_file_path
+        self.validation_file_path=validation_file_path
         self.verbose=verbose
         self.valid_list=valid_list
         self.ignore_list=ignore_list
 
-    def scan_master_folder(self):
-            return os.listdir(self.master_folder_path)
+    def scan_master_folder(self) -> List[str]:
+        return os.listdir(self.master_folder_path)
 
-    def log_header_to_file(self, header="act_id"):
+    def trim_master_folder(self) -> Tuple(List[str], List[str]):
+        try:
+            with open(f"{self.validation_file_path}") as val_file:
+                self.valid_list = json.load(val_file)
+                for path in self.valid_list:
+                    if os.isdir(path):
+                        pass
+                    else:
+                        self.ignore_list.append(path)
+        except:
+            print(f"Error reading validation file: {self.validation_file_path}")
+        finally:
+            return self.valid_list, self.ignore_list
+
+    def log_header_to_file(self, header="act_id") -> bool:
         """
         csv_header = act_id
         """
-        with open(f"{self.log_file_path}", "w") as log_file:
-            log_file.write(header + "\n")
-    
-    def log_to_file(self):
-        with open(f"{self.log_file_path}", "a") as log_file:
-            for path in self.scan_master_folder().filter(lambda folder : os.isdir(folder)):
-                log_file.write(f"{path}")
+        try:
+            with open(f"{self.log_file_path}", "w") as log_file:
+                if self.verbose==1:
+                    print(f"Writing header into log file: {self.log_file_path}")
+                log_file.write(header + "\n")
+        except:
+            print(f"Error writing header into log file: {self.log_file_path}")
+        finally:
+            return True
+
+    def log_to_file(self) -> bool:
+        try:
+            if not self.valid_list:
+                with open(f"{self.log_file_path}", "a") as log_file:
+                    for path in self.scan_master_folder().filter(lambda folder : os.isdir(folder)):
+                        if self.verbose==1:
+                            print(f"Writing act_id record into log file: {self.log_file_path}")
+                    log_file.write(f"{path}")
+            else:
+                with open(f"{self.log_file_path}", "a") as log_file:
+                    for path in self.valid_list:
+                        if self.verbose==1:
+                            print(f"Writing act_id record into log file: {self.log_file_path}")
+                    log_file.write(f"{path}")
+        except:
+            print(f"Error writing act_id info into log file: {self.log_file_path}")
+        finally:
+            return True
 
 class DatasetFormatter():
     def __init__(self):
