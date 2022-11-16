d97bdc40ea27a344303cab94ca28df7523311681
wibox <francesco.info.pagano@gmail.com>
2022-11-16 14:16:01 +0100

    DataScanner setup.

--- main/Preprocessing/dataloader.py
+++ main/Preprocessing/dataloader.py
@@ -1,16 +1,48 @@
 """
 QUESTO FILE CONTIENE LA CLASSE NECESSARIA A SCANNERIZZARE TUTTE LE INFORMAZIONI RELATIVE AL DATASET
 A NOSTRA DISPOSIZIONE E SALVA TUTTI I RECORD IN UN FILE .CSV APPOSITAMENTE FORMATTATO(cocks)
+
+IL WORKFLOW E' IL SEGUENTE:
+    - PRENDO IMMAGINE DA /pre (RICOSTRUIRE TILES)
+    - PRENDO IMMAGINE DA /post (RICOSTRUIRE TILES)
+    - ASSEMBLO PRE E POST E LI METTO IN /complete (sono già tile-assembled)
+    - APPLICO LA MASCHERA A QUELLA COMPLETA
+
 """
+
+import os
+import sys
+import glob
+
+
 class DatasetScanner():
-    def __init__(self):
-        pass
+    """
+    Legge dalla master folder le cartelle all'interno delle quali ci sono pre, post e mask.
+    Successivamente genera un csv con tutte le informazioni relative alla singola attivazione.
+    In questo modo evitiamo di mantenere in memoria a runtime 520 giga di informazioni sul path assoluto
+    E processiamo riga per riga il csv risultate. (generato da log_to_file()).
+    """
+    def __init__(self, master_folder_path=None, log_file_path=None, verbose=0, valid_list=None, ignore_list=None):
+        self.master_folder_path=master_folder_path
+        self.log_file_path=log_file_path
+        self.verbose=verbose
+        self.valid_list=valid_list
+        self.ignore_list=ignore_list
 
     def scan_master_folder(self):
-        pass
+            return os.listdir(self.master_folder_path)
 
-    def log_to_file(self, filename):
-        pass
+    def log_header_to_file(self, header="act_id"):
+        """
+        csv_header = act_id
+        """
+        with open(f"{self.log_file_path}", "w") as log_file:
+            log_file.write(header + "\n")
+    
+    def log_to_file(self):
+        with open(f"{self.log_file_path}", "a") as log_file:
+            for path in self.scan_master_folder().filter(lambda folder : os.isdir(folder)):
+                log_file.write(f"{path}")
 
 class DatasetFormatter():
     def __init__(self):
@@ -19,6 +51,5 @@ class DatasetFormatter():
     def check_dimensions(self):
         pass
 
-    def format_dimensions(self):
+    def tiling(self):
         pass
-
--- main/interface.py
+++ main/interface.py
@@ -1,1 +1,12 @@
-import argparse
+from Preprocessing.dataloader import DatasetScanner
+
+import os
+
+ds = DatasetScanner(
+    master_folder_path="/hdd/effis-wildfire",
+    log_file_path=os.join.path("/logs", "act_id.csv")
+)
+
+ds.scan_master_folder()
+ds.log_header_to_file()
+ds.log_to_file()
