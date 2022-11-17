283aade85d6e2cc22e5148e02c2f530901cb1107
wibox <francesco.info.pagano@gmail.com>
2022-11-17 15:34:57 +0100

    ImageDataset and DatasetFormatter completed.

--- main/Preprocessing/dataloader.py
+++ main/Preprocessing/dataloader.py
@@ -1,6 +1,14 @@
 import os
+import itertools
 import json
+
 from typing import *
+
+from tqdm import tqdm
+
+import rasterio as rio
+
+from torch.utils.data.dataset import Dataset
 
 class DatasetScanner():
     """
@@ -54,13 +62,13 @@ class DatasetScanner():
                 with open(f"{self.log_file_path}", "a") as log_file:
                     for path in self.scan_master_folder().filter(lambda folder : os.isdir(folder)):
                         if self.verbose==1:
-                            print(f"Writing act_id record into log file: {self.log_file_path}")
+                            print(f"Writing {path} record into log file: {self.log_file_path}")
                     log_file.write(f"{path}")
             else:
                 with open(f"{self.log_file_path}", "a") as log_file:
                     for path in self.valid_list:
                         if self.verbose==1:
-                            print(f"Writing act_id record into log file: {self.log_file_path}")
+                            print(f"Writing {path} record into log file: {self.log_file_path}")
                     log_file.write(f"{path}")
         except:
             print(f"Error writing act_id info into log file: {self.log_file_path}")
@@ -68,11 +76,146 @@ class DatasetScanner():
             return True
 
 class DatasetFormatter():
-    def __init__(self):
+    """
+    Questa classe effettua un check sulla dimensione di ogni immagine in input (presa da validated_list di DataScanner). 
+        - Se la dimensione è giusta -> tiling
+        - Se la dimensione è sbagliata -> padding/overlapping -> tiling
+        - Ogni immagine viene poi salvata in una directory con la seguente struttura:
+            - act_id
+                - pre:
+                    - act_tiles
+                - post:
+                    - act_tiles
+                - mask:
+                    - mask_tiles
+    """
+    def __init__(self, master_folder_path=None, log_file_path=None, log_filename=None, master_dict_path=None, valid_list=None, tile_height=512, tile_width=512, use_pre=False, verbose=0):
+        self.master_folder_path=master_folder_path
+        self.log_file_path=log_file_path
+        self.log_filename=log_filename
+        self.master_dict_path=master_dict_path
+        self.valid_list=valid_list
+        self.tile_height=tile_height
+        self.tile_width=tile_width
+        self.use_pre=use_pre
+        self.verbose=verbose
+
+        self.master_dict_info = {"processing_info": []}
+
+    def log_dict_info(self,
+    act_id : str,
+    tiles_info : List[str],
+    subitem : str
+     ) -> Dict:
+        logging_info = {
+            f"{act_id}" : {
+                f"tiles_info_{subitem}" : tiles_info
+            }
+        }
+        self.master_dict_info["processing_info"].append(logging_info)
+        return self.master_dict_info
+
+    def check_dimensions(self) -> bool:
+        """
+        Implementare questa funzione non ha senso se rasterio effettua overlapping in automatico. Da verificare.
+        """
         pass
 
-    def check_dimensions(self):
-        pass
+    def get_tiles(self, ds):
+        ncols, nrows = ds.meta['width'], ds.meta['height']
+        offsets = itertools.product(range(0, ncols, self.tile_width), range(0, nrows, self.tile_height))
+        big_window = rio.windows.Window(col_off=0,
+                                    row_off=0,
+                                    width=ncols,
+                                    height=nrows)
+        for col_off, row_off in offsets:
+            window = rio.windows.Window(col_off=col_off, row_off=row_off, width=self.tile_width, height=self.tile_height).intersection(big_window)
+            transform = rio.windows.transform(window, ds.transform)
+            yield window, transform
 
-    def tiling(self):
+    def tiling(self) -> List[str]:
+        #input_path = os.join.path(self.master_folder_path, act_path)
+        input_paths = list()
+
+        #log_info
+        # per act_paths usiamo direttamente input_paths
+        tiles_info_pre_list = list()
+        tiles_info_post_list = list()
+        tiles_info_mask_list = list()
+
+        if self.use_pre:
+            input_paths_subitems = ["pre/", "post/", "mask/"]
+        else:
+            input_paths_subitems = ["post/", "mask/"]
+
+        try:
+            with open(f"{self.log_file_path}/{self.log_filename}", "r") as act_paths_file:
+                for act_path in act_paths_file:
+                    input_paths.append(act_path)
+        except:
+            print(f"Error building useful activations list.")
+        finally:
+            if self.verbose==1:
+                print("Finished building input paths list. Proceeding.")
+
+        for idx in tqdm(range(len(input_paths))):
+            tiles_info = list()
+            current_processing_folder=f"processed_{input_paths[idx]}"
+            os.mkdir(f"processed_{input_paths[idx]}")
+            if self.verbose==1:
+                print(f"Working with: {input_paths[idx]}")
+                print(f"Creating processing folder: processed_{input_paths[idx]}")
+            for subitem in input_paths_subitems:
+                with rio.open(os.path.join(input_paths[idx], subitem)) as inds:
+
+                    #tile_width, tile_height = self.tile_width, self.tile_height
+
+                    meta = inds.meta.copy()
+
+                    #qui creo la cartella di output delle tiles
+                    os.mkdir(f"{current_processing_folder}/{subitem}")
+                    output_path = f"{current_processing_folder}/{subitem}"
+                    output_filename = 'tile_{}_{}.tif'
+
+                for window, transform in self.get_tiles(inds):
+                    print(window)
+                    meta['transform'] = transform
+                    meta['width'], meta['height'] = window.width, window.height
+                    outpath = os.path.join(output_path,output_filename.format(int(window.col_off), int(window.row_off)))
+                    with rio.open(outpath, 'w', **meta) as outds:
+                        outds.write(inds.read(window=window))
+                        tiles_info.append(inds.read(window=window))
+
+
+                self.log_dict_info(
+                    act_id=input_paths[idx],
+                    tiles_info=tiles_info,
+                    subitem=subitem
+                )
+
+                    #qui cancello la cartella originale dopo aver fatto la lavorazione
+                os.rmdir(input_paths[idx])
+
+        json.dumps(self.log_dict_info, self.master_dict_path)
+            
+class ImageDataset(Dataset):
+    def __init__(self, processed_master_folder_path=None, master_dict_path=None, tile_width=512, tile_height=512, transform=None, use_pre=False, verbose=0):
+        self.processed_master_folder_path=processed_master_folder_path
+        self.master_dict_path=master_dict_path
+        self.tile_width=tile_width
+        self.tile_heigth=tile_height
+        self.transform=transform
+        self.use_pre=use_pre
+        self.verbose=verbose
+    
+    def __len__(self) -> int:
+        try:
+            with open(f"{self.master_dict_path}", "r") as jsonfp:
+                info_dict = json.load(jsonfp)
+        except:
+            print(f"Error dealing with log json file at: {self.master_dict_path}")
+        finally:
+            return len(info_dict)
+
+    def __getitem__(self):
         pass
