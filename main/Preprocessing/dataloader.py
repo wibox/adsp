67ad00cb5bd2f2341780c7cf595841a324f05b4c
giuseppeEsposito98 <giuseppeesposito98@libero.it>
2022-11-25 10:53:25 +0100

    creata funzione per overlappare le tiles

--- main/Preprocessing/dataloader.py
+++ main/Preprocessing/dataloader.py
@@ -7,6 +7,7 @@ from typing import *
 from tqdm import tqdm
 
 import rasterio as rio
+import numpy as np
 
 from torch.utils.data.dataset import Dataset
 
@@ -89,7 +90,7 @@ class DatasetFormatter():
                 - mask:
                     - mask_tiles
     """
-    def __init__(self, master_folder_path=None, log_file_path=None, log_filename=None, master_dict_path=None, valid_list=None, tile_height=512, tile_width=512, use_pre=False, verbose=0):
+    def __init__(self, master_folder_path=None, log_file_path=None, log_filename=None, master_dict_path=None, valid_list=None, tile_height=512, tile_width=512, thr_pixels = 112, use_pre=False, verbose=0):
         self.master_folder_path=master_folder_path
         self.log_file_path=log_file_path
         self.log_filename=log_filename
@@ -97,6 +98,7 @@ class DatasetFormatter():
         self.valid_list=valid_list
         self.tile_height=tile_height
         self.tile_width=tile_width
+        self.thr_pixels = thr_pixels
         self.use_pre=use_pre
         self.verbose=verbose
 
@@ -115,19 +117,84 @@ class DatasetFormatter():
         self.master_dict_info["processing_info"].append(logging_info)
         return self.master_dict_info
 
-    def check_dimensions(self) -> bool:
+    ## Context:  tutto questo viene fatto mentre fai il tiling
+    # controlla il numero di dimensioni delle immagini
+    # se non ce ne sono abbastanza aumenta gli assi
+    # controlla i valori delle dimensioni,
+    # int(ratio dell'altezza per 512) = numero di tile che ci vengono interi
+    # ratio della larghezza per 512 = numero di tile che ci vengono interi
+    # dato un threshold,
+    # se il la tile ha larghezza o lunghezza minore del threshold
+    # discarda la tile 
+    # altrimenti
+    # calcola quanti pixel mancano dall'avere l'immagine delle dimensioni desiderate (overlapping)
+    # e per capire da che pixel partire per completare l'ultima tile 
+    # fine dell'ultima tile intera - i pixel di overlapping. 
+
+    
+    def offset_tiles(self, img: np.ndarrary, tile_dim= [512, 512]) -> Tuple[List[float], List[float]]:
         """
-        Implementare questa funzione non ha senso se rasterio effettua overlapping in automatico. Da verificare.
+        Suppose to have arrays formatted as channel first
+        Questa funzione controlla le dimensioni dell'immagine data in ingresso, nel caso in cui il numero della dimensione
+        non fosse quello desiderarto aggiungine una (questo è per le maschere che saranno in bianco e nero, un canale). 
+        Una volta aggiustato il numero di dimensioni, fai il check sui valori delle dimensioni e nel caso in cui non 
+        siano divisibili per 512 fai l'overlapping dell'ultima tile che rimane con dimensioni non giuste con la penultima tile
+        img: ndarray
+        thr: minimum pixels are needed to create another tile
         """
-        pass
+        if len(img.shape) == 2:
+            axis = 0
+            img = np.expand_dims(img, axis = axis)
+            # da aggiungere se vogliamo i channel alla fine.
+            #img = np.moveaxis(img, 0, -1)
+        # cambiare questo se non è channel first
+        _, heigth, width = img.shape
+        tile_h, tile_w = tile_dim
+
+        # check for the heigth
+        # dato che abbiamo immagini minimo di 512 x 512 non ci preoccupiamo di fare un upsampling
+        if heigth % tile_h == 0:
+            num_tile_h = int(heigth/tile_h)
+            offsets_h = [i*tile_h for i in range(num_tile_h)]
+        elif heigth % tile_h != 0:
+            # controlla prima il resto quant'è remaining = resto
+            remaining_h = heigth % tile_h
+            if remaining_h > self.thr_pixels:
+                num_tile_h = int(np.floor(heigth/tile_h)) + 1 
+            else:
+                num_tile_h = int(np.floor(heigth/tile_h))
+            offsets_h = [i*tile_h for i in range(num_tile_h-1)]
+            offsets_h.append(offsets_h[-1] - (tile_h - remaining_h))
+        
+        # check for the width
+        # dato che abbiamo immagini minimo di 512 x 512 non ci preoccupiamo di fare un upsampling
+        if width % tile_w == 0:
+            num_tile_w = int(width/tile_w)
+            offsets_w = [i*tile_w for i in range(num_tile_w)]
+        elif width % tile_w != 0:
+            # controlla prima il resto quant'è remaining = resto
+            remaining_w = width % tile_w
+            if remaining_w > self.thr_pixels:
+                num_tile_w = int(np.floor(width/tile_w)) + 1
+            else:
+                num_tile_w = int(np.floor(width/tile_w))
+            offsets_w = [i*tile_w for i in range(num_tile_w-1)]
+            offsets_w.append(offsets_w[-1] - (tile_w - remaining_w))
+
+        return offsets_w, offsets_h
 
     def get_tiles(self, ds):
+        # shape = (c, h, w) questa è l'immagine intera
+        img_as_np = ds.read()
         ncols, nrows = ds.meta['width'], ds.meta['height']
-        offsets = itertools.product(range(0, ncols, self.tile_width), range(0, nrows, self.tile_height))
+        #offsets = itertools.product(range(0, ncols, self.tile_width), range(0, nrows, self.tile_height))
+        offsets_w, offsets_h = self.offset_tiles(img_as_np)
+        offsets = itertools.product(offsets_w, offsets_h)
         big_window = rio.windows.Window(col_off=0,
                                     row_off=0,
                                     width=ncols,
                                     height=nrows)
+
         for col_off, row_off in offsets:
             window = rio.windows.Window(col_off=col_off, row_off=row_off, width=self.tile_width, height=self.tile_height).intersection(big_window)
             transform = rio.windows.transform(window, ds.transform)
@@ -217,5 +284,22 @@ class ImageDataset(Dataset):
         finally:
             return len(info_dict)
 
-    def __getitem__(self):
-        pass
+    def __getitem__(self, idx):
+        """
+        Questa funzione prende in input un indice relativo all'immagine e alla maschera che vogliamo caricare sull'algoritmo da trainare
+        e, tramite l'indice, carica la relativa immagine e maschera e le ritorna in un dizionario.
+        """
+        local_dict = dict()
+
+        local_dict['post_image_tile'] = self.master_dict[f"act_id_{idx}"]["tile_info_post"]
+        local_dict['mask_tile'] = self.master_dict[f"act_id_{idx}"]["tile_info_mask"]
+        if self.use_pre:
+            local_dict['pre_image_tile'] = self.master_dict[f"act_id_{idx}"]["tile_info_pre"]
+
+        return local_dict
+    
+
+    
+
+
+
