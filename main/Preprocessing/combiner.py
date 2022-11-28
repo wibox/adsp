e2365b94d3f2e4f69bb746d22c242ed41f234636
wibox <francesco.info.pagano@gmail.com>
2022-11-28 17:27:34 +0100

    NNs defined. Interface.py structured.

--- main/Preprocessing/combiner.py
+++
@@ -1,9 +0,0 @@
-def Combiner():
-    def __init__():
-        pass
-
-    def combine(self, pre, post, mask):
-        pass
-
-    def save_combined_img(self, img):
-        pass
--- main/interface.py
+++ main/interface.py
@@ -2,6 +2,16 @@ from Preprocessing.dataloader import DatasetScanner
 
 import os
 
+# Mettere un parser.
+# Inserire la logica di inizializzazione degli oggetti
+### QUI INIZIA IL MAIN ###
+# Inizializzare i vari dataset con i vari split (per il dataset che abbiamo scelto dal parser)
+# Inizializzare la rete scelta (dal parser)
+# Trainare la rete scelta 
+# Computare le metriche in output
+
+
+
 ds = DatasetScanner(
     master_folder_path="/hdd/effis-wildfire",
     log_file_path=os.join.path("/logs", "act_id.csv")
