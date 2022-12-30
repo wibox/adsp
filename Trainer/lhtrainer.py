8de7958fd784faa0215d3ffe438b4ac5cc891717
wibox <francesco.info.pagano@gmail.com>
2022-12-30 10:36:26 +0100

    elp

--- Trainer/lhtrainer.py
+++ Trainer/lhtrainer.py
@@ -70,6 +70,6 @@ class Trainer():
 
         if best_iou_score < valid_logs['iou_score']:
             best_iou_score = valid_logs['iou_score']
-            torch.save(self.model, "/mnt/data1/adsp_data/best_model.pt")
+            torch.save(self.model, "/mnt/data1/adsp_data/best_model.mdl")
             print("Saving model...")
 
