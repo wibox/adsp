927ce4d8dd84794681ac0f9bf61b7406df4fcce7
wibox <francesco.info.pagano@gmail.com>
2023-01-04 16:37:02 +0100

    loss changed

--- test.py
+++ test.py
@@ -184,7 +184,7 @@ val_ds = image_dataset.ImageDataset(
 
 epochs = 15
 device = 'cuda'
-loss = utils.losses.DiceLoss()
+loss = utils.losses.BCEWithLogitsLoss()
 optimizer = torch.optim.Adam(
         [dict(params=model.parameters(), lr=1e-4)]
 )
